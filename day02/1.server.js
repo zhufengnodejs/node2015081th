892501118376f0200115dd8222094264a1490b2f
zhangrenyang-t510 <zhang_renyang@>
2015-11-18 21:59:34 +0800

    2

--- day02/1.server.js
+++ day02/1.server.js
@@ -1,16 +1,38 @@
 var http = require('http');
+var fs = require('fs');
+var formidable = require('formidable');
 /**
  * @param request 请求对象
  * @param response 响应对象
  */
+var filename ;
 function start(request,response){
     var url = request.url;//从请求里获取请求url
-    if(url =='/hello'){
-        response.write('hello');//向客户端说话
-    }else if(url =='/world'){
-        response.write('world');
+    if(url =='/'){
+        var fileContent = fs.readFileSync('./index.html');//读取文件的内容
+        response.write(fileContent);//写入响应
+        response.end();//结束响应
+    }else if(url =='/upload'){
+        var parser = new formidable.IncomingForm();
+        //把请求里的元素数据取出来传入回调函数 fileds普通文件字段  files放着所有的文件
+        parser.parse(request,function(err,fileds,files){
+            console.log(files);
+            //files.myfile.path
+            // type: 'image/jpeg',
+            //name: '2015年5月26日redis基础.jpg',
+            //从临时目录读取文件的内容
+            var filecontent = fs.readFileSync(files.myfile.path);
+           //把文件的内容写到当前文件夹下面,文件名叫 files.myfile.name
+            fs.writeFileSync('./'+files.myfile.name,filecontent);
+            response.write(files.myfile.name);
+            filename = files.myfile.name;
+            response.end();
+        });
+    }else if(url =='/'+filename){
+        var fileContent = fs.readFileSync('./'+filename);//读取文件的内容
+        response.write(fileContent);//写入响应
+        response.end();//结束响应
     }
-    response.end();//结束响应，
 }
 var server = http.createServer(start);
 
